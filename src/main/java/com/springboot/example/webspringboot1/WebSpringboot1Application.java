0deae992e0152ad4811d472e6877b7b26ad42ee1
Jeonghyunkyo <hchk11@naver.com>
2022-12-15 13:00:58 +0900

    Initial commit

---
+++ src/main/java/com/springboot/example/webspringboot1/WebSpringboot1Application.java
@@ -0,0 +1,13 @@
+package com.springboot.example.webspringboot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpringboot1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSpringboot1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/example/webspringboot1/WebSpringboot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.example.webspringboot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSpringboot1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
